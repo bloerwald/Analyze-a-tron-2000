b97660bf8e9b42f74f821aa08f53a595fa4f8425
Bernd Lörwald <bloerwald@googlemail.com>
2020-07-11 01:28:17 +0200

    ida: tcontainers: only state name once

--- ida/tcontainers.py
+++ ida/tcontainers.py
@@ -4,8 +4,8 @@ class blz_vector (tutil.template_description):
   def __init__ (self):
     tutil.template_description.__init__ (self, "blz::vector", ["T"])
   def create_types (self, parameter_names, unique = True):
-    tutil.add_packed_type ( "_2_blz_vector",
-                            "unsigned long long size; unsigned long long capacity;")
+    p2 = tutil.add_packed_type ( "_2_blz_vector",
+                                 "unsigned long long size; unsigned long long capacity;")
     tutil.add_packed_type ( self.make_name (parameter_names),
-                            "%s* data; _2_blz_vector _2;" % (parameter_names[0]),
+                            "%s* data; %s _2;" % (parameter_names[0], p2),
                             unique)
